fc71bf2b25d81b0b5a63535eeb21a3d2197c5835
shinwr7 <shinwr7@naver.com>
2021-10-19 18:15:03 +0900

    1019 ICT after

--- widgetPrj4/app/src/main/java/com/ict/widgetprj4/MainActivity.java
+++ widgetPrj4/app/src/main/java/com/ict/widgetprj4/MainActivity.java
@@ -8,12 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn1;
     EditText et1;
-
+    TextView txt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,18 +24,22 @@ public class MainActivity extends AppCompatActivity {
 
         btn1 = (Button)findViewById(R.id.btn1);
         et1 = (EditText)findViewById(R.id.et1);
-
+        txt1 = (TextView)findViewById(R.id.txt1);
         // 버튼에 온클릭설정
         btn1.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Log.d("click","클릭감지");
                 String Result = et1.getText().toString();
                 Log.d("et1값", Result);
+                // 얻어온 값을 Toast로 찍어보기
+                Toast.makeText(MainActivity.this, Result, Toast.LENGTH_SHORT).show();
+
+                txt1.setText(Result);
             }
+
         });
-
-
 
     }
 
